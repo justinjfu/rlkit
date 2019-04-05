9c9080cf33e7ce9147af7d0421f743a16e5bdfe3
Justin Fu <justinjfu@gmail.com>
2019-04-05 16:01:39 -0700

    updates

--- examples/search.py
+++ examples/search.py
@@ -4,7 +4,7 @@ Run PyTorch Soft Actor Critic on HalfCheetahEnv.
 NOTE: You need PyTorch 0.3 or more (to have torch.distributions)
 """
 import numpy as np
-from gym.envs.mujoco import HalfCheetahEnv
+from rlkit.envs.mbpo.halfcheetah import HalfCheetahEnv
 
 import rlkit.torch.pytorch_util as ptu
 from rlkit.envs.wrappers import NormalizedBoxEnv
@@ -16,25 +16,47 @@ from rlkit.torch.networks import FlattenMlp
 import heapdict
 
 class Path(object):
-    def __init__(self):
+    def __init__(self, init_state=None):
         self.states = []
+        if init_state:
+            self.states.append(init_state)
+        self.returns = 0
 
-def search():
+    def extend(self, action, next_state, reward):
+        new_path = Path()
+        new_path.returns = reward + self.returns
+        new_path.states = self.states + [next_state]
+        return new_path
+
+def aggregate(state, granularity=10):
+    # the higher the granularity, the more fine the discretization
+    rounded = np.floor(state * granularity).astype(np.int32)
+    return tuple(rounded[1:])
+
+def search(env):
     heap = heapdict.heapdict()
     visited = set()
-    key = (state, 0, None)
+
+    start_state = env.sim_reset()
+    key = (state, Path(state))
     heap[key] = 0
 
-    while len(heap)>0:
+    for _ in range(10000):
         (state, path), priority = heap.popitem()
-        if state in visited:
-            continue
-        visited.add(state)
 
-        #
-        #cur_heuristic = heuristic(state, goal)
-        for succ, succ_action in succs:
-            priority = 0
+        agg_state = aggregate(state)
+        if agg_state in visited:
+            raise NotImplementedError("need to update paths??")
+        visited.add(agg_state)
+
+        # sample actions
+        sampled_actions = [env.action_space.sample() for _ in range(5)]
+        for action in sampled_actions:
+            next_state = env.sim_transition(state, action)
+            reward = env.sim_reward(state, action, next_state)
+            new_path = path.extend(action, next_state, reward)
+
+            priority = -new_path.returns
             key = (succ, new_path)
             heap[key] = priority
 
--- rlkit/envs/mbpo/halfcheetah.py
+++ rlkit/envs/mbpo/halfcheetah.py
@@ -6,6 +6,9 @@ class HalfCheetahEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
         mujoco_env.MujocoEnv.__init__(self, 'half_cheetah.xml', 5)
         utils.EzPickle.__init__(self)
+        
+        self.qpos_idx = slice(0,self.model.nq)
+        self.qvel_idx = slice(self.model.nq,self.model.nv)
 
     def step(self, action):
         #xposbefore = self.sim.data.qpos[0]
@@ -17,19 +20,31 @@ class HalfCheetahEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         done = False
         return nob, reward, done, {}
 
-    def reward(self, o, action, no):
-        #xposbefore = o[0]
-        #xposafter = no
+    def sim_reset(self):
+        qpos = self.init_qpos + self.np_random.uniform(low=-.1, high=.1, size=self.model.nq)
+        qvel = self.init_qvel + self.np_random.randn(self.model.nv) * .1
+        return np.c_[qpos, qvel]
+
+    def sim_transition(self, state, action):
+        self.set_state(state)
+        self.do_simulation(action, self.frame_skip)
+        return self.sim.data.qpos
+
+    def sim_reward(self, state, action, next_state):
         reward_ctrl = - 0.1 * np.square(action).sum()
-        #reward_run = (xposafter - xposbefore)/self.dt
-        #import pdb; pdb.set_trace()
-        vel1 = o[8]
-        vel2 = no[8]
-        reward_run = (vel1+vel2)/2.0
+        xposbefore = state[0]
+        xposafter = next_state[0]
+        reward_run = (xposafter-xposbefore)/self.dt
         reward = reward_ctrl + reward_run
         return reward
 
-    def _get_obs(self):
+    def sim_obs(self, state):
+        return np.concatenate([
+            state[self.qpos_idx].flat[1:],
+            state[self.qvel_idx].flat,
+        ])
+
+    def _get_obs(self, state):
         return np.concatenate([
             self.sim.data.qpos.flat[1:],
             self.sim.data.qvel.flat,
